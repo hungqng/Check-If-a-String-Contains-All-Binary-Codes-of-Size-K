67c33b93547db3c4491b7024adc968f1d15b9c27
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-30 23:03:01 -0700

    Add files via upload

---
+++ Check If a String Contains All Binary Codes of Size K.py
@@ -0,0 +1,6 @@
+# 1461. Check If a String Contains All Binary Codes of Size K
+
+# Given a binary string s and an integer k, return true if every binary code of length k is a substring of s. Otherwise, return false.
+
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
